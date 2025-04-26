116c276d12332c0ad620835b80a48011f7f422b8
Aaruni Arora <aaruni.arora@gmail.com>
2025-04-26 19:24:33 +0100

    updates to the test function

--- BMI_Coursework_JamiBananas.mat/testFunction_for_students_MTb.m
+++ BMI_Coursework_JamiBananas.mat/testFunction_for_students_MTb.m
@@ -73,15 +73,3 @@ RMSE = sqrt(meanSqError/n_predictions)
 rmpath(genpath(teamName))
 
 end
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-disp(['Making sure mean RMSE here is the same as their formula: ' num2str(mean(meanRMSE_perBin))]);
-disp(['Overall classification accuracy: ' num2str(mean(accuracy_perBin)*100)])
-
-if ~isempty(figname), save_figure(gcf, 'figures', figname, 'pdf', 'vector', false); end
-end
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
